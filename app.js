e0ac2b751c1903b6ba792996afb2706f89886ddb
Wanderer <82797317+wanderer00a1@users.noreply.github.com>
2025-03-22 00:22:53 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
-if(process.env.NODE_ENV !== "production"){
-  require('dotenv').config();
-}
 
-// require('dotenv').config();
+
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const path = require('path');
